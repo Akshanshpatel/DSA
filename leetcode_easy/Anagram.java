d93b4c195e98e96678ad13c0c413c18bdac6322d
Akshansh Patel <142593097+Akshanshpatel@users.noreply.github.com>
2024-12-05 23:28:52 +0530

    Update Anagram.java

--- leetcode_easy/Anagram.java
+++ leetcode_easy/Anagram.java
@@ -16,9 +16,9 @@ class Solution {
      public static HashMap<Character, Integer> populateHashMap(String str) {
         HashMap<Character, Integer> charFrequencyMap = new HashMap<>();
         
-        for (char c : str.toCharArray()) {
-            charFrequencyMap.put(c, charFrequencyMap.getOrDefault(c, 0) + 1);
-        }
+        for (char c : str.toCharArray()) {                        //.toCharArray iterate c for every character in string eg-"literal"-> ['l','i','t','e','r','a','l']
+            charFrequencyMap.put(c, charFrequencyMap.getOrDefault(c, 0) + 1);  //.put(stores the value of character in hashmap)
+        }                                                                      //.getOrdefault(get character value in string or default(0) if not found)+1
         
         return charFrequencyMap;
     }
